2820c8abd1280278f4d0507864c58bb5fa41b03e
Your Name <you@example.com>
2022-08-03 20:43:54 +0530

    [Megha] Add: Welcome to Employee Wage Computation

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeewageComputation
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welome To Employee Wage Computation");
+        }
+    }
+}
