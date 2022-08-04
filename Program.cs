85955bf628724b47a735cac5da778f47d2194824
Your Name <you@example.com>
2022-08-04 15:27:21 +0530

    [Megha] Add: UC1 Added

--- Program.cs
+++ Program.cs
@@ -1,10 +1,15 @@
-﻿namespace EmployeewageComputation
+﻿using EmployeeWageComputation;
+
+namespace EmployeewageComputation
 {
-    class Program
-    {
+   class Program
+   {
         public static void Main(string[] args)
         {
             Console.WriteLine("Welome To Employee Wage Computation");
+
+            EmployeeCheck employee = new EmployeeCheck();
+            employee.CheckEmployeeAttendance();
         }
-    }
+   }
 }
