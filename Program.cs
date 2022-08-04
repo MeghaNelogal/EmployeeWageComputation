0fcb5fcaa1e3fba7eb3c23a2950f334ed089cb86
Your Name <you@example.com>
2022-08-04 15:46:07 +0530

    [Megha] Add: UC4 Added

--- Program.cs
+++ Program.cs
@@ -7,7 +7,6 @@ namespace EmployeewageComputation
         public static void Main(string[] args)
         {
             Console.WriteLine("Welome To Employee Wage Computation");
-
             EmployeeCheck employee = new EmployeeCheck();
             employee.CheckEmployeeAttendance();
         }
