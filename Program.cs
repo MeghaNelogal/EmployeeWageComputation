34b7654c539ae101fd21709f4ed0c342e5ab23a1
Your Name <you@example.com>
2022-08-07 23:34:30 +0530

    [Megha] Add: Added UC12

--- Program.cs
+++ Program.cs
@@ -9,14 +9,7 @@ namespace EmployeewageComputation
             Console.WriteLine("Welome To Employee Wage Computation");
 
             CalculateMonthlyWage company = new CalculateMonthlyWage();
-            Console.WriteLine("Enter :\nThe Company Name\nEmpWage Working Per Hours\nTotal Working Hours\nTotal Working Days In Month");
-            string compName = Console.ReadLine();
-            int EmpWageWorkingPerHours = Convert.ToInt32(Console.ReadLine());
-            int TotalWorkingHours = Convert.ToInt32(Console.ReadLine());
-            int TotalWorkingDaysInMonth = Convert.ToInt32(Console.ReadLine());
-            company.AddCompanyEmpWage(compName, EmpWageWorkingPerHours, TotalWorkingHours, TotalWorkingDaysInMonth);
             company.ComputeEmpWage();
-            Console.WriteLine("Total wage for" + compName + ":" + " " + company.GetTotalEmpWage(compName));
         }
    }
 }
