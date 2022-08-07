943b534d06769c1c1e98d91482e2a91c3409c4d9
Your Name <you@example.com>
2022-08-07 18:39:53 +0530

    [Megha] Add: Added UC9

--- Program.cs
+++ Program.cs
@@ -7,11 +7,16 @@ namespace EmployeewageComputation
         public static void Main(string[] args)
         {
             Console.WriteLine("Welome To Employee Wage Computation");
-         
-            CalculateMonthlyWage employee = new CalculateMonthlyWage();          
-            employee.Company("Samsung", 20, 4, 20);
-            employee.Company("Reliance", 10, 4, 15);
-            employee.Company("Airtel", 30, 5, 20);
+
+            CalculateMonthlyWage samsung = new CalculateMonthlyWage("Samsung", 20, 5, 10);
+            CalculateMonthlyWage reliance = new CalculateMonthlyWage("Reliance", 10, 4, 15);
+            CalculateMonthlyWage airtel = new CalculateMonthlyWage("Airtel", 30, 5, 20);
+            samsung.ComputeEmployeeWage();
+            Console.WriteLine(samsung.displaySalary() + "\n");
+            reliance.ComputeEmployeeWage();
+            Console.WriteLine(reliance.displaySalary() + "\n");
+            airtel.ComputeEmployeeWage();
+            Console.WriteLine(airtel.displaySalary() + "\n");
         }
    }
 }
