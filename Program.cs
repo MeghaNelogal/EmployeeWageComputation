8a74fb89dfbd6d524989671a211d07174e14b0b6
Your Name <you@example.com>
2022-08-07 18:33:43 +0530

    [Megha] Add: Added UC8

--- Program.cs
+++ Program.cs
@@ -7,11 +7,11 @@ namespace EmployeewageComputation
         public static void Main(string[] args)
         {
             Console.WriteLine("Welome To Employee Wage Computation");
-
-            EmployeeCheck employee = new EmployeeCheck();
-            employee.CheckEmployeeAttendance();
-            CalculateMonthlyWage monthlyWage = new CalculateMonthlyWage();
-            monthlyWage.CalculateMonthlyEmoployeeWage();
+         
+            CalculateMonthlyWage employee = new CalculateMonthlyWage();          
+            employee.Company("Samsung", 20, 4, 20);
+            employee.Company("Reliance", 10, 4, 15);
+            employee.Company("Airtel", 30, 5, 20);
         }
    }
 }
