cce0424dd832a5de6bc31a93a41968f5e08be0fe
Your Name <you@example.com>
2022-08-07 23:41:52 +0530

    [Megha] Add: Added UC13

--- Program.cs
+++ Program.cs
@@ -7,7 +7,6 @@ namespace EmployeewageComputation
         public static void Main(string[] args)
         {
             Console.WriteLine("Welome To Employee Wage Computation");
-
             CalculateMonthlyWage company = new CalculateMonthlyWage();
             company.ComputeEmpWage();
         }
